8276ace7933138568988480d8113625f0dce0b9f
Ahmaddddd7 <ahmadtariq.malik@yahoo.com>
2026-09-09 23:02:28 +0500

    Completed Lab 1 tasks and worksheet

--- CA LAB1/task 3.s
+++ CA LAB1/task 3.s
@@ -2,18 +2,18 @@
 .globl main
 main:
     
-    li x01, 5   
-    li x02, 0         #a = x1
-    addi x02, x02, 0      #b = x2
-    addi x01, x02, 32     
-    add x03, x01, x02           #d = x3
-    addi x03, x03, -5         
-    sub x04, x01, x03             #e = x4
-    sub x05, x02, x01                    #(b-a) = x5
-    add x04, x04, x05
-    add x04, x04, x03
-    add x04, x01, x04
-    add x04, x04, x02
-    add x04, x04, x03
+    li x1, 5   
+    li x2, 0         #a = x1
+    addi x2, x2, 0      #b = x2
+    addi x1, x2, 32     
+    add x3, x1, x2           #d = x3
+    addi x3, x3, -5         
+    sub x4, x1, x3             #e = x4
+    sub x5, x2, x1                    #(b-a) = x5
+    add x4, x4, x5
+    add x4, x4, x3
+    add x4, x1, x4
+    add x4, x4, x2
+    add x4, x4, x3
 end:
     j end   
--- CA LAB1/task 4b.s
+++ CA LAB1/task 4b.s
@@ -2,29 +2,51 @@
 .globl main
 main:
 
-    li x01, 0x100
-    li x02, 0x200
-    li x03, 0x300
+    li x1, 0x100
+    li x2, 0x200
+    li x3, 0x300
 
-    lb x20, 0(x01)
-    lh x21, 0(x02)
-    add x22, x20, x21
-    sw x23, 0(x03)
 
-    lb x20, 1(x01)
-    lh x21, 2(x02)
-    add x22, x20, x21
-    sw x23, 4(x03)
+    li x5, 10
+    sb x5, 0(x1)        #a[0] = 10
+    li x5, 20
+    sb x5, 1(x1)        #a[1] = 20
+    li x5, 30
+    sb x5, 2(x1)        #a[2] = 30
+    li x5, 40
+    sb x5, 3(x1)        #a[3] = 40
 
-    lb x20, 2(x01)
-    lh x21, 4(x02)
-    add x22, x20, x21
-    sw x23, 8(x03)
 
-    lb x20, 3(x01)
-    lh x21, 6(x02)
+    li x6, 10
+    sh x6, 0(x2)        #b[0] = 10
+    li x6, 20
+    sh x6, 2(x2)        #b[1] = 20
+    li x6, 30
+    sh x6, 4(x2)        #b[2] = 30
+    li x6, 40
+    sh x6, 6(x2)        #b[3] = 40
+
+
+
+    lb x20, 0(x1)
+    lh x21, 0(x2)
     add x22, x20, x21
-    sw x23, 12(x03)
+    sw x22, 0(x3)
+
+    lb x20, 1(x1)
+    lh x21, 2(x2)
+    add x22, x20, x21
+    sw x22, 4(x3)
+
+    lb x20, 2(x1)
+    lh x21, 4(x2)
+    add x22, x20, x21
+    sw x22, 8(x3)
+
+    lb x20, 3(x1)
+    lh x21, 6(x2)
+    add x22, x20, x21
+    sw x22, 12(x3)
 
 end:
     j end 
